fc8221717a68ba31aeb628caeaf9d8e48175f01d
Hossain <arafat.kd99@gmail.com>
2024-03-05 12:31:14 +0100

    Add BrouserRouter, Routes & Route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrouserRouter as Router, Routes } from 'react-router-dom';
+import { BrouserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );

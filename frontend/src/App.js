0999c9d3bbdc7c06e9a869a52e7ede0ab8a820a3
Hossain <arafat.kd99@gmail.com>
2024-03-06 00:47:07 +0100

    Add Nav component to Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
 import Home from './components/Home';
 import './styles/App.css';
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/about" element={<About />} /> */}

6713fbcd2ede5fa8ade09de80c20f259c85c5cdf
Hossain <arafat.kd99@gmail.com>
2024-03-09 13:39:57 +0100

    Update readme file

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.valid?
       token = encode_token({ user_id: @user.id })
-      render json: { user: @user, token: token }, status: :created
+      render json: { user: @user, token: }, status: :created
     else
       render json: { error: 'Invalid username or password' }, status: :not_acceptable
     end
@@ -15,9 +15,9 @@ class UsersController < ApplicationController
   # Login
   def login
     @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
+    if @user&.authenticate(params[:password])
       token = encode_token({ user_id: @user.id })
-      render json: { user: @user, token: token, redirect_to: '/localhost:3000/dashboard' }, status: :accepted
+      render json: { user: @user, token:, redirect_to: '/localhost:3000/dashboard' }, status: :accepted
     else
       render json: { error: 'Invalid username or password' }, status: :unauthorized
     end
@@ -33,5 +33,4 @@ class UsersController < ApplicationController
   def user_params
     params.permit(:username, :password, :age)
   end
-
 end

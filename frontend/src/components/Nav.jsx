d08e5969d4bc6616728e2d887068ee88911140a0
Hossain <arafat.kd99@gmail.com>
2024-03-10 23:28:12 +0100

    Update nav button style

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -25,8 +25,8 @@ function Nav() {
           <Link to="/page3">Page-3</Link>
           {!isLoggedIn && (
             <>
-              <Link to="/signup"><button type="button">Sign Up</button></Link>
-              <Link to="/login"><button type="button">Login</button></Link>
+              <Link to="/signup"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button></Link>
+              <Link to="/login"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button></Link>
             </>
           )}
           {isLoggedIn && (

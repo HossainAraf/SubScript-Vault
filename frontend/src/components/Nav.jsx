8a7c6e1790a7d769ca65b1fe65af6490d0aeb826
Hossain <arafat.kd99@gmail.com>
2024-03-10 12:52:21 +0100

    Comment-out ssl-bind since key & cert are linked with local machine & not for production purpose

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,26 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function Nav() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
-    // Get the buttons
-    const signupButton = document.getElementById('signup-btn');
-    const loginButton = document.getElementById('login-btn');
-    const logoutButton = document.getElementById('logout-btn');
-
-    // Get the token
+    // Check if the user is logged in
     const token = localStorage.getItem('token');
-
-    // If the token exists, show the logout button, otherwise show the signup and login buttons
-    if (token) {
-      logoutButton.style.display = 'block';
-      signupButton.style.display = 'none';
-      loginButton.style.display = 'none';
-    } else {
-      logoutButton.style.display = 'none';
-      signupButton.style.display = 'block';
-      loginButton.style.display = 'block';
-    }
+    setIsLoggedIn(!!token); // Update isLoggedIn state based on the presence of token
   }, []); // Run only once when the component mounts
 
   return (
@@ -28,9 +15,17 @@ function Nav() {
       <ul>
         <li>
           <Link to="/">Home</Link>
-          <Link to="/signup"><button id="signup-btn" type="button">Sign Up</button></Link>
-          <Link to="/login"><button id="login-btn" type="button">Login</button></Link>
-          <Link to="/logout"><button id="logout-btn" type="button">Logout</button></Link>
+          {!isLoggedIn && (
+            <>
+              <Link to="/signup"><button type="button">Sign Up</button></Link>
+              <Link to="/login"><button type="button">Login</button></Link>
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              <Link to="/logout"><button type="button">Logout</button></Link>
+            </>
+          )}
         </li>
       </ul>
     </nav>

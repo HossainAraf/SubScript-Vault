e4b880f1fb4a06b05a7f8236c649ea3aa2fea59e
Hossain <arafat.kd99@gmail.com>
2024-03-14 02:18:10 +0100

    Update elements style of dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -12,45 +12,49 @@ import sample8 from '../assets/img/sample-8.jpg';
 import sample9 from '../assets/img/sample-9.jpg';
 
 const Dashboard = () => (
-  <div className="dashboard grid grid-cols-3 gap-4">
-    <h1>Dashboard</h1>
-    <div className="w-full">
-      <h3>Content -1</h3>
-      <img src={sample1} alt="sample-1" className="w-full h-full object-cover" />
+  <div className="dashboard-wrapper">
+    <div className="header-wrapper">
+      <h1>Dashboard</h1>
     </div>
-    <div className="w-full">
-      <h3>Content -2</h3>
-      <img src={sample2} alt="sample-2" className="w-full h-full object-cover" />
+    <div className="content-wrapper grid grid-cols-3 gap-4 space-y-4">
+      <div className="w-full">
+        <img src={sample1} alt="sample-1" className="w-full h-full object-cover" />
+        <h3>Content -1</h3>
+      </div>
+      <div className="w-full">
+        <img src={sample2} alt="sample-2" className="w-full h-full object-cover" />
+        <h3>Content -2</h3>
+      </div>
+      <div className="w-full">
+        <img src={sample3} alt="sample-3" className="w-full h-full object-cover" />
+        <h3>Content -3</h3>
+      </div>
+      <div className="w-full">
+        <img src={sample4} alt="sample-4" className="w-full h-full object-cover" />
+        <h3>Content -4</h3>
+      </div>
+      <div className="w-full">
+        <img src={sample5} alt="sample-5" className="w-full h-full object-cover" />
+        <h3>Content -5 </h3>
+      </div>
+      <div className="w-full">
+        <img src={sample6} alt="sample-6" className="w-full h-full object-cover" />
+        <h3>Content -6</h3>
+      </div>
+      <div className="w-full">
+        <img src={sample7} alt="sample-7" className="w-full h-full object-cover" />
+        <h3>Content -7</h3>
+      </div>
+      <div className="w-full">
+        <img src={sample8} alt="sample-8" className="w-full h-full object-cover" />
+        <h3>Content -8</h3>
+      </div>
+      <div className="w-full">
+        <img src={sample9} alt="sample-9" className="w-full h-full object-cover" />
+        <h3>Content -9</h3>
+      </div>
+      <Logout />
     </div>
-    <div className="w-full">
-      <h3>Content -3</h3>
-      <img src={sample3} alt="sample-3" className="w-full h-full object-cover" />
-    </div>
-    <div className="w-full">
-      <h3>Content -4</h3>
-      <img src={sample4} alt="sample-4" className="w-full h-full object-cover" />
-    </div>
-    <div className="w-full">
-      <h3>Content -5 </h3>
-      <img src={sample5} alt="sample-5" className="w-full h-full object-cover" />
-    </div>
-    <div className="w-full">
-      <h3>Content -6</h3>
-      <img src={sample6} alt="sample-6" className="w-full h-full object-cover" />
-    </div>
-    <div className="w-full">
-      <h3>Content -7</h3>
-      <img src={sample7} alt="sample-7" className="w-full h-full object-cover" />
-    </div>
-    <div className="w-full">
-      <h3>Content -8</h3>
-      <img src={sample8} alt="sample-8" className="w-full h-full object-cover" />
-    </div>
-    <div className="w-full">
-      <h3>Content -9</h3>
-      <img src={sample9} alt="sample-9" className="w-full h-full object-cover" />
-    </div>
-    <Logout />
   </div>
 );
 

9eb86cf1ee1fc6db59a75e0b157058ee34f6bfd4
Hossain <arafat.kd99@gmail.com>
2024-03-17 20:42:24 +0100

    Fix render issue

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -30,5 +30,7 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # Disable asset compilation
+    config.assets.enabled = false
   end
 end
